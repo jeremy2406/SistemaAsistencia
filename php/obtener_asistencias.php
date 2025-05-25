07a4182afac0d06077ced543d9b9d8ef27efe85a
jeremy2406 <diazjeremy344@gmail.com>
2025-05-25 18:44:23 -0400

    actualizacion
    reparo en los require

--- php/obtener_asistencias.php
+++ php/obtener_asistencias.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: Content-Type');
 
-require_once '../config.php';
+require_once './php/config.php';
 
 try {
     $supabase = new SupabaseClient();
